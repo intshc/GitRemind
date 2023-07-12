17ab2d919cf87989b85ee4417e8f6e218179be09
Hyeongchan Shin <intshc@gmail.com>
2023-07-12 14:35:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/gitremind/GitRemindApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRemindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitremind/GitRemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitremind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
