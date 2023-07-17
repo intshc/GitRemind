f48ae29d4ac47104c4a55bd4185835bcf096354b
Hyeongchan Shin <intshc@gmail.com>
2023-07-17 17:44:18 +0900

    컨트롤러 파라미터로 입력받기

--- src/main/java/com/example/gitremind/controller/LoginController.java
+++ src/main/java/com/example/gitremind/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.gitremind.controller;
 
+import com.example.gitremind.dto.CreateUserDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class LoginController {
     @GetMapping("/login")
     public String login(){
         return "로그인 페이지";
+    }
+    @GetMapping("/login-form")
+    public CreateUserDto login(@RequestParam String username, @RequestParam String email){
+        return CreateUserDto.builder()
+                .username(username)
+                .email(email).build();
     }
 }
--- src/test/java/com/example/gitremind/controller/LoginControllerTest.java
+++ src/test/java/com/example/gitremind/controller/LoginControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class LoginControllerTest {
@@ -23,5 +22,23 @@ class LoginControllerTest {
         mockMvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("로그인 페이지"));
+    }
+
+    @Test
+    @DisplayName("유저 파라미터로 입력받기")
+    void userParam() throws Exception{
+        //given
+        String name = "사용자";
+        String email = "intshc@gmail.com";
+
+        //expect
+        mockMvc.perform(get("/login-form")
+                        .param("username",name)
+                        .param("email",email))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value(name))
+                .andExpect(jsonPath("$.email").value(email));
+
     }
 }
