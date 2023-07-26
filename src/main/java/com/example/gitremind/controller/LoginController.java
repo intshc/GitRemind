b986d60feb6ef41562cfd1d0627d9fe6d862726b
Hyeongchan Shin <intshc@gmail.com>
2023-07-26 19:20:18 +0900

    cors 문제 해결, 로그인 성공 시에 '/user/api' 경로에 정보 담기, 초창기 login 테스트 삭제

--- src/main/java/com/example/gitremind/controller/LoginController.java
+++ src/main/java/com/example/gitremind/controller/LoginController.java
@@ -1,21 +1,35 @@
 package com.example.gitremind.controller;
 
-import com.example.gitremind.dto.CreateUserDto;
+import com.example.gitremind.domain.User;
+import com.example.gitremind.dto.SessionUser;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(){
-        return "로그인 페이지";
+    private final HttpSession httpSession;
+
+    @GetMapping("/user/api")
+    public ResponseEntity<SessionUser> getData() {
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        if(sessionUser != null){
+            return ResponseEntity.ok(sessionUser);
+        }
+        return ResponseEntity.notFound().build();
     }
-    @GetMapping("/login-form")
-    public CreateUserDto login(@RequestParam String username, @RequestParam String email){
-        return CreateUserDto.builder()
-                .username(username)
-                .email(email).build();
+
+    @GetMapping("/")
+    public void redirect(HttpServletResponse response) throws Exception {
+        String redirect_uri = "http://localhost:3000/";
+        response.sendRedirect(redirect_uri);
     }
+
 }
